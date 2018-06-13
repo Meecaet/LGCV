c1ad9579ecbe888b3d41257e17e91dfc757fcaf7
Mateus E. E. Caetano <meecaet@gmail.com>
2018-06-13 10:20:48 -0400

    Documents "lisez-moi" et quelques commentaires ont été ajoutés

--- ConvertisseurDeCV/PrototypeDuConvertisseur/Program.cs
+++ ConvertisseurDeCV/PrototypeDuConvertisseur/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using XmlHandler.Generation;
 
 namespace PrototypeDuConvertisseur
 {
@@ -6,7 +7,15 @@ namespace PrototypeDuConvertisseur
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Appuyer sur un bouton pour demarrer");
+            Console.ReadKey();
+
+            CVGenerator generator = new CVGenerator();
+            generator.ProcessCV(args[0]);
+
+            Console.WriteLine("Convertion finalisée");
+            Console.WriteLine("Appuyer sur un bouton pour sortir");
+            Console.ReadKey();
         }
     }
 }
--- ConvertisseurDeCV/XmlHandler/XmlIdentification/IXmlToken.cs
+++ ConvertisseurDeCV/XmlHandler/XmlIdentification/IXmlToken.cs
@@ -9,6 +9,12 @@ namespace XmlHandler.XmlIdentification
 {
     public interface IXmlToken
     {
+        /// <summary>
+        /// Méthode qui dit si un nœud répresent le début d'un section
+        /// </summary>
+        /// <param name="node">Nœud en train d'être lu</param>
+        /// <param name="identificant">Non de la section qui, dans le cas où le "match" est vrai, sera retourné</param>
+        /// <returns>Indicatif si le nœud passe ou non par la véerification</returns>
         bool Match(XmlNode node, out string identificant);
     }
 }
