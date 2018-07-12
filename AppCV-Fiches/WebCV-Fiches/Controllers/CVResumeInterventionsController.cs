3defb8ef4be5cf0567ee701116d25311e273c7ac
jdchicangana <jdchicangana@QDI-4688>
2018-07-12 09:08:57 -0400

    Controller et viewmodel pour resume d'interventions.

---
+++ AppCV-Fiches/WebCV-Fiches/Controllers/CVResumeInterventionsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebCV_Fiches.Models.CVViewModels;
+
+namespace WebCV_Fiches.Controllers
+{
+    [Route("ResumeIntervention")]
+    public class CVResumeInterventionsController : Controller
+    {
+        [Route("{cvId}/All")]
+        [AllowAnonymous]
+        public ActionResult All(string utilisateurId)
+        {
+            return Json(new List<ResumeInterventionViewModel>());
+        }
+    }
+}
---
+++ AppCV-Fiches/WebCV-Fiches/Models/CVViewModels/ResumeInterventionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCV_Fiches.Models.CVViewModels
+{
+    public class ResumeInterventionViewModel
+    {
+        public int Nombre { get; set; }
+        public string Client { get; set; }
+        public string project { get; set; }
+        public int Envergure { get; set; }
+        public string fonction { get; set; }
+        public int annee { get; set; }
+        public int effors { get; set; }
+    }
+}
