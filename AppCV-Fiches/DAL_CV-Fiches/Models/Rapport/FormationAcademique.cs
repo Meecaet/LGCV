ae46f3e6bcfc52a3a9cbe831a45ee507be9eb54e
Mateus E. E. Caetano <meecaet@gmail.com>
2018-07-16 18:00:07 -0400

    Ajout de génération de document docx depuis les données

---
+++ AppCV-Fiches/DAL_CV-Fiches/Models/Rapport/FormationAcademique.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL_CV_Fiches.Models.Rapport
+{
+    public class FormationAcademique
+    {
+        public int Id { get; set; }
+        public string Diplome { get; set; }
+        public int AnDuTitre { get; set; }
+        public string Instituition { get; set; }       
+    }
+}
---
+++ AppCV-Fiches/DAL_CV-Fiches/Models/Rapport/TechnologieCV.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL_CV_Fiches.Models.Rapport
+{
+    public class TechnologieCV
+    {
+    }
+}
