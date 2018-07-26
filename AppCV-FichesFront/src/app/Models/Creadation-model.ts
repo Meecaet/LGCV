890e3eb8e3199c3891c59197134b01ca973685a2
Juan Pablo Bustamante <jpbustamante@LGSDEV.COM>
2018-07-26 16:53:58 -0400

    Modifier le navbar

--- AppCV-FichesFront/src/app/Models/Creadation-model.ts
+++ AppCV-FichesFront/src/app/Models/Creadation-model.ts
@@ -2,7 +2,8 @@ export class Credential{
      authenticated :boolean;
      created :Date;
      expiration :Date;
-     token :string
-     message :string
-     utilisateurId:string
+     token :string;
+     message :string;
+     utilisateurId:string;
+     userName:string;
 }
